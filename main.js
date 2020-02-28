3d76684539fb4b0ddbc05684bb4fb2ddaf59fb41
Andrew Apple <rep.andrewapple@yahoo.com>
2020-02-28 12:34:05 -0600

    Add images

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var thumbBar = document.querySelector('.thumb-bar');
   //var pic = ["pic1", "pic2", "pic3", "pic4", "pic5"]
   //var img = " "
 
-for (var i = 1; i <= 5; i++){
+for (var i = 6; i <= 10; i++){
     //img = img + i;
   var newImage = document.createElement('img');
   newImage.setAttribute('src', 'images/pic' + i + '.jpg');
